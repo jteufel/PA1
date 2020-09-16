5b4a864858d95730d0c24ba5e4882aa318295782
Justin Teufel <jteufel208@gmail.com>
2020-09-16 13:11:43 -0600

    fixed compilation errors

---
+++ .my_driver.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/user/Desktop/PA1/my_driver.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/user/Desktop/PA1/my_driver.ko /home/user/Desktop/PA1/my_driver.o /home/user/Desktop/PA1/my_driver.mod.o ;  true
--- my_driver.c
+++ my_driver.c
@@ -3,29 +3,29 @@
 #include<linux/init.h>
 #include<linux/module.h>
 #include<linux/fs.h>
-#include<asm/uaccess.h>
-#include <errno.h>
+#include<linux/uaccess.h>
 
 #define buffer_length 1024
-static char kernal_buffer[buffer_length]
+static char kernal_buffer[buffer_length];
 
-int open_count = 0;
-int release_count = 0;
+//int open_count = 0;
+//int release_count = 0;
 
 
 ssize_t my_read(struct file *my_file, char *buffer, size_t count, loff_t *offset)
 {
 
 	int to_read;
+	int ctu;
 
 	if (buffer_length > count)
 			to_read = count;
-	else
+	elseint ctu
 			to_read = count;
 
 	//copy_to_user
 	//The function returns zero on success or non-zero to indicate the number of bytes that weren'ttransferred.
-	int ctu = copy_to_user(buffer, kernal_buffer, to_read);
+	ctu = copy_to_user(buffer, kernal_buffer, to_read);
 
 	//error if bytes aren't transferred?
 	//if(ctu != 0) return -1;
@@ -43,15 +43,16 @@ ssize_t my_write(struct file *my_file, const char *buffer, size_t count, loff_t 
 {
 
 	int to_write;
+	int cfu;
 
 	if (buffer_length > count)
 			to_write = count;
 	else
-			buffer_length = count;
+			to_write = buffer_length;
 
 	//copy_from_user
 	//The function returns zero on success or non-zero to indicate the number of bytes that weren'ttransferred.
-	int cfu = copy_from_user(kernal_buffer, buffer, to_write);
+	cfu = copy_from_user(kernal_buffer, buffer, to_write);
 
 	//error if bytes aren't transferred?
 	//if(ctu != 0) return -1;
@@ -66,7 +67,8 @@ ssize_t my_write(struct file *my_file, const char *buffer, size_t count, loff_t 
 };
 
 int my_open(struct inode *pinode, struct file *my_file)
-{
+{	
+	int open_count;
 	printk(KERN_ALERT "Opening simple_character_device\n");
 	open_count++;
 	printk(KERN_ALERT "Device has been opened %d times\n",open_count);
@@ -75,7 +77,8 @@ int my_open(struct inode *pinode, struct file *my_file)
 };
 
 int my_release(struct inode *pinode, struct file *my_file)
-{
+{	
+	int release_count;
 	printk(KERN_ALERT "Releasing simple_character_device\n");
 	release_count++;
 	printk(KERN_ALERT "Device has been released %d times\n",release_count);
@@ -97,15 +100,15 @@ static struct file_operations my_device_operations = {
 int my_driver_init(void)
 {
 	printk(KERN_ALERT "inside %s function\n",__FUNCTION__);
-	register_chrdev(240,"simple_character_device", &file_operations);
+	register_chrdev(240,"simple_character_device", &my_device_operations);
+	return 0;
 };
 
 
-int my_driver_exit(void)
+void my_driver_exit(void)
 {
 	printk(KERN_ALERT "inside %s function\n",__FUNCTION__);
 	unregister_chrdev(240,"simple_character_device");
-	return 0;
 };
 
 
