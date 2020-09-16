d64e9c32b749f079cdb1e368347a728c97a93bcd
jteufel <jteufel208@gmail.com>
2020-09-16 16:41:19 -0600

    implemented ll_seek

--- my_driver.c
+++ my_driver.c
@@ -4,12 +4,15 @@
 #include<linux/module.h>
 #include<linux/fs.h>
 #include<linux/uaccess.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define buffer_length 1024
+#define maj 240
 static char kernal_buffer[buffer_length];
 
 int open_count = 0;
-int release_count = 0;
+offset_t file_pointer_location = 0;
 
 
 ssize_t my_read(struct file *my_file, char *buffer, size_t count, loff_t *offset)
@@ -22,20 +25,16 @@ ssize_t my_read(struct file *my_file, char *buffer, size_t count, loff_t *offset
 	if (buffer_length > count)
 			to_read = count;
 	else
-			to_read = count;
+			to_read = buffer_length;
 
 	//copy_to_user
 	//The function returns zero on success or non-zero to indicate the number of bytes that weren'ttransferred.
 	ctu = copy_to_user(buffer, kernal_buffer, to_read);
 
-	//error if bytes aren't transferred?
-	//if(ctu != 0) return -1;
-	//errno =
-
-	//number of
 	result = to_read - ctu;
 
 	printk(KERN_ALERT "Device has read %d bytes\n",result);
+
 	return result;
 
 };
@@ -56,34 +55,45 @@ ssize_t my_write(struct file *my_file, const char *buffer, size_t count, loff_t 
 	//The function returns zero on success or non-zero to indicate the number of bytes that weren'ttransferred.
 	cfu = copy_from_user(kernal_buffer, buffer, to_write);
 
-	//error if bytes aren't transferred?
-	//if(ctu != 0) return -1;
-	//errno =
-
-	//number of
 	result = to_write - cfu;
 
 	printk(KERN_ALERT "Device has written %d bytes\n",result);
+
 	return result;
 
 };
 
+offset_t my_llseek(int fildes, offset_t offset, int whence)
+{
+
+	switch (whence) {
+		case SEEK_SET:
+			file_pointer_location = offset;
+		case SEEK_CUR:
+			file_pointer_location = offset + file_pointer_location;
+		case SEEK_END:
+			file_pointer_location = buffer_length - offset;
+	};
+
+	return file_pointer_location;
+};
+
 int my_open(struct inode *pinode, struct file *my_file)
-{	
+{
 	//int open_count;
 	printk(KERN_ALERT "Opening simple_character_device\n");
+
 	open_count++;
+
 	printk(KERN_ALERT "Device has been opened %d times\n",open_count);
 
 	return 0;
 };
 
 int my_release(struct inode *pinode, struct file *my_file)
-{	
+{
 	//int release_count;
 	printk(KERN_ALERT "Releasing simple_character_device\n");
-	release_count++;
-	printk(KERN_ALERT "Device has been released %d times\n",release_count);
 
 	return 0;
 };
@@ -95,14 +105,15 @@ static struct file_operations my_device_operations = {
 	.open = my_open,
 	.release = my_release,
 	.read = my_read,
-	.write = my_write
+	.write = my_write,
+	.llseek = my_llseek
 
 };
 
 int my_driver_init(void)
 {
 	printk(KERN_ALERT "inside %s function\n",__FUNCTION__);
-	register_chrdev(240,"simple_character_device", &my_device_operations);
+	register_chrdev(maj,"simple_character_device", &my_device_operations);
 	return 0;
 };
 
@@ -110,7 +121,7 @@ int my_driver_init(void)
 void my_driver_exit(void)
 {
 	printk(KERN_ALERT "inside %s function\n",__FUNCTION__);
-	unregister_chrdev(240,"simple_character_device");
+	unregister_chrdev(maj,"simple_character_device");
 };
 
 
