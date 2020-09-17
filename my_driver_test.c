fcd6c263b23bf14bda7a71e1b040d97dde3bec8f
jteufel <jteufel208@gmail.com>
2020-09-16 21:59:02 -0600

    test changes

--- my_driver_test.c
+++ my_driver_test.c
@@ -22,9 +22,15 @@ printf("Read again\n");
 read(my_device, kernal_buffer, BUFFER);
 printf("Read output: %s\n", kernal_buffer);
 
+loff_t seek_off = 20;
+
 printf("Testing llseek with SEEK_SET\n");
-int set = llseek(my_device, 20, SEEK_SET);
+loff_t set = llseek(my_device, seek_off, SEEK_SET);
 printf("llseek output: %s\n", set);
+
+printf("Testing llseek with SEEK_CUR\n");
+loff_t cur = llseek(my_device, seek_off, SEEK_CUR);
+printf("llseek output: %s\n", cur);
 
 close(my_device);
 
